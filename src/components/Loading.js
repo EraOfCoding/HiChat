0304d1e47031243e2f49a0aeb8134fe8e82bf044
EraOfCoding <63988030+EraOfCoding@users.noreply.github.com>
2021-05-10 18:10:51 +0600

    send me interesting facts please

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,7 +5,9 @@ function Loading() {
     const phrases = [
         "Did you know that creator is a 13 years old student?",
         "By the way do you know what Karlson is?",
-        "Please give me 100% on SAT uwu"
+        "Please give me 100% on SAT xd",
+        "Tatakae, tatakae means fight, fight",
+        "Did you know that I used React and firebase to create this web app?"
     ]
 
     const phrase = phrases[Math.floor(Math.random() * phrases.length)]
