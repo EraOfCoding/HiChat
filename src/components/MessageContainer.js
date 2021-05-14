a657047c1e329494fc58cd45fdb556fdf567d93c
EraOfCoding <63988030+EraOfCoding@users.noreply.github.com>
2021-05-14 18:24:38 +0600

    link update

--- src/components/MessageContainer.js
+++ src/components/MessageContainer.js
@@ -2,12 +2,19 @@ import React from 'react';
 import '../style/MessageContainer.css'
 
 function MessageContainer({ key = null, text = "", profile = null, username = "", time = [] }) {
+    let ghost = ""
     return (
         <div key={key} className="msg-container">
             <img className="profile" src={profile} />
             <div className="msg">
                 <h6 className="username">{username} <p>{time}</p></h6>
-                <p className="text" key={key}>{text}</p>
+                <p className="text" key={key}>
+                    {text.split(' ').map(
+                        word => word.startsWith('https://') ?
+                            <pre><a className="link" key={key} onClick={() => window.open(text)}> {word}</a></pre> :
+                            <pre> {word}</pre>
+                    )}
+                </p>
             </div>
         </div>
     )
