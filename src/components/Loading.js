91c5f655a1cea6a494207eaeabc0be5d880968f9
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-22 14:10:47 +0600

    feature: add a couple new phrases to the loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,10 +5,12 @@ import calculateAge from '../utils/calculateAge'
 function Loading() {
     const phrases = [
         `Did you know that the creator is a ${calculateAge()} year old student?`,
+        "Every 60 seconds in Africa a minute passes.",
+        "A wise man once said: Hippity hoppity your code now is my property.",
         "By the way do you know what Karlson is?",
         "Please give me 100% on SAT xd",
         "Tatakae, tatakae means fight, fight",
-        "Did you know that I used React and firebase to create this web app?"
+        "Did you know that I used React and Firebase to create this web app?"
     ]
 
     const phrase = phrases[Math.floor(Math.random() * phrases.length)]
